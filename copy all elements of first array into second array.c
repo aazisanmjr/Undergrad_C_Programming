60e954f3c92d6b8c0a392790acb3416d83f5696f
T I ZISAN <zisan13407@gmail.com>
2021-12-15 09:00:57 +0600

    u

---
+++ copy all elements of first array into second array.c
@@ -0,0 +1,43 @@
+/*10.	Write a C program to input elements in array and copy all elements of first array into second array.
+Example
+Input
+Input array1 elements: 10 1 95 30 45 12 60 89 40 -4
+
+Output
+Array1: 10 1 95 30 45 12 60 89 40 -4
+Array2: 10 1 95 30 45 12 60 89 40 -4
+*/
+#include <stdio.h>    
+     
+int main()    
+{    
+    //Initialize array     
+    int arr1[] = {1, 2, 3, 4, 5};    
+        
+    //Calculate length of array arr1    
+    int length = sizeof(arr1)/sizeof(arr1[0]);    
+        
+    //Create another array arr2 with the size of arr1.    
+    
+    int arr2[length];    
+        
+    //Copying all elements of one array into another    
+    for (int i = 0; i < length; i++) {     
+        arr2[i] = arr1[i];     
+    }      
+        
+    //Displaying elements of array arr1     
+    printf("Elements of original array: \n");    
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", arr1[i]);    
+    }    
+        
+    printf("\n");    
+        
+    //Displaying elements of array arr2     
+    printf("Elements of new array: \n");    
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", arr2[i]);    
+    }    
+    return 0;    
+}    
